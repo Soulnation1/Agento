6235a6e3964eca41b091c341e8772e6cdb8cb8e5
Ogundare Oluwaseun <ogundareoluwaseunayodeji@gmail.com>
2026-03-09 07:36:30 -0700

    button components merging

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,30 @@
 import clsx from "clsx";
-const Button = ({title, type, size, onClick, className}) => {
-  
-    const style = clsx(
-         " text-[#ffffff] py-3 rounded",
-         type === "danger" && "bg-[#fff0f0] hover:bg-red-100 text-[#e04040] border border-[#f8d8d8] rounded-md ",
-        type === "success" && "bg-green-500 hover:bg-green-600 text-white",
-        type === "warning" && "bg-yellow-500 hover:bg-yellow-600 text-white", 
-        type === "common" && "bg-[#9680fb] rounded-xl  text-[#ffffff] hover:bg-[#7f63ff] hover:scale-105 transition font-semibold  ",
-        type === "compose" && "bg-[#f0f0fc] text-[#6c65ff] border border-[#ddddf8] px-3 py-1.5 rounded-xl  hover:scale-105 transition ",
-        size === "sm" && "px-2 py-2 ",
-        size === "x-small" && "px-8 py-[3px] ",
-        size === "md" && "px-10 py-1.5 ",
-        size === "lg" && "px-6 py-3 ",
-        size === "regular" && "w-full py-[8px]",
-        className,
-    )
+const Button = ({ title, type, size, onClick, className }) => {
+  const style = clsx(
+    " text-[#ffffff] py-3 rounded",
+    type === "danger" &&
+      "bg-[#fff0f0] hover:bg-red-100 text-[#e04040] border border-[#f8d8d8] rounded-md ",
+    type === "success" && "bg-green-500 hover:bg-green-600 text-white",
+    type === "warning" && "bg-yellow-500 hover:bg-yellow-600 text-white",
+    type === "common" &&
+      "bg-[#9680fb] rounded-xl  text-[#ffffff] hover:bg-[#7f63ff] hover:scale-105 transition font-semibold  ",
+    type === "compose" &&
+      "bg-[#f0f0fc] text-[#6c65ff] border border-[#ddddf8] px-3 py-1.5 rounded-xl  hover:scale-105 transition ",
+    size === "sm" && "px-2 py-2 ",
+    size === "x-small" && "px-8 py-[3px] ",
+    size === "md" && "px-10 py-1.5 ",
+    size === "lg" && "px-6 py-3 ",
+    size === "regular" && "w-full py-[8px]",
+    className,
+  );
   return (
     <div>
-<button className={style} onClick={onClick}>
-      {title}
-    </button>
+      <button className={style} onClick={onClick}>
+        {title}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 // color
 // red-danger
@@ -36,4 +37,4 @@ const Button = ({title, type, size, onClick, className}) => {
 // medium-md
 // large-lg
 
-export default Button
+export default Button;
