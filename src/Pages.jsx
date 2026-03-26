916ba04c4597f8cdc758e3ff3d293f5c810636f7
Ogundare Oluwaseun <ogundareoluwaseunayodeji@gmail.com>
2026-03-25 22:10:38 -0700

    Revert "Merge branch 'feat/register' into main"
    This reverts commit 6968d334c89819d2b3487c7b88dfded7547c351c, reversing
    changes made to 07eb2509d15094a013e81eaaaaa89feb640bd077.

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import NavBar from "./navbar/NavBar";
-import SlideOne from "./slides/SlideOne";
-import SlideTwo from "./slides/SlideTwo";
-import SignUpSlide from "./slides/SignUpSlide";
+import React from 'react'
+import NavBar from './navbar/NavBar'
+import SlideOne from './slides/SlideOne'
+import SlideTwo from './slides/SlideTwo'
+import SignUpSlide from './slides/SignUpSlide'
 // import SignInSlide from './slides/SingInSlide'
 
 const Pages = () => {
   return (
-    <div className="bg-[#eaf4ee] w-full ">
-      <NavBar />
-      <SlideOne />
-      <SlideTwo />
-      <SignUpSlide />
+    <div className='bg-[#eaf4ee] w-full '>
+        <NavBar/>
+        <SlideOne/>
+        <SlideTwo/>
+        <SignUpSlide/>
+        {/* <SignInSlide/> */}
      
     </div>
-  );
-};
+  )
+}
 
-export default Pages;
+export default Pages
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -28,22 +28,3 @@ const Button = ({ title, types, size, onClick, disabled, className }) => {
 };
 
 export default Button;
-const Button = ({ onClick,title }) => {
-
-  interface ButtonProps {
-    title: string;
-    onClick?: () => void;
-    size?: "small" | "medium" | "large";
-    type?: "primary" | "secondary";
-  }
-
-"bg-"
-
-  return (
-    <button  className="bg-white  text-blue-500 p-10" onClick={onClick}>
-    {title}
-    </button>
-  )
-}
-
-export default Button
